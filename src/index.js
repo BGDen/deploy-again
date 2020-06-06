7e00f1a9e3f9d544f11c738e8bbb02253d445ef1
Den <fentau.line@gmail.com>
2020-06-06 11:53:37 +0300

    Create next step

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
-import './main.css'
-
 import React from 'react'
 import ReactDOM from 'react-dom'
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
 import { createBrowserHistory } from 'history'
-import { routerMiddleware } from 'connected-react-router'
+import { routerMiddleware } from 'connected-react-router' // ConnectedRouter
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { HashRouter } from 'react-router-dom'
 
 import routes from 'routes'
 import createRootReducer from 'reducers'
 
+import Grid from '@material-ui/core/Grid'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import Container from '@material-ui/core/Container'
+import { brown } from '@material-ui/core/colors'
 
 
 const history = createBrowserHistory()
@@ -23,11 +25,19 @@ const store = createStore(
 )
 
 ReactDOM.render(
-  <Provider store={store}>
-    <HashRouter history={history}>
-      {routes}
-    </HashRouter>
-  </Provider>,
+  <React.Fragment>
+    <CssBaseline />
+    <Container maxWidth='xl' style={{padding: 0, width: '100vw', height: '100vh'}}>
+      <Grid container direction="row" justify="center" alignItems="center" style={{height: '100%', backgroundColor: brown[100]}}>
+        <Provider store={store}>
+          <HashRouter history={history}>
+            {routes}
+          </HashRouter>
+        </Provider>
+      </Grid>
+    </Container>
+  </React.Fragment>
+  ,
   document.getElementById('root')
 )
 
